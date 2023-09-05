b13e103441bea198895dc8a53834c5b54d4980c8
FredyF3113 <104546111+FredyF3113@users.noreply.github.com>
2023-09-04 21:56:06 -0500

    ArmarioController.java

--- src/main/java/pe/edu/upc/clothingfashion/controllers/ArmarioController.java
+++ src/main/java/pe/edu/upc/clothingfashion/controllers/ArmarioController.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/armarios")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class ArmarioController {
     @Autowired
     private IArmarioService aS;
