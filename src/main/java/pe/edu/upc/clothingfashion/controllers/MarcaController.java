7ac6df1a9f25a178730ec1b44e4821edfd4fcf8f
FredyF3113 <104546111+FredyF3113@users.noreply.github.com>
2023-09-04 21:58:39 -0500

    MarcaController.java

--- src/main/java/pe/edu/upc/clothingfashion/controllers/MarcaController.java
+++ src/main/java/pe/edu/upc/clothingfashion/controllers/MarcaController.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/marcas")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class MarcaController {
     @Autowired
     private IMarcaService mS;
