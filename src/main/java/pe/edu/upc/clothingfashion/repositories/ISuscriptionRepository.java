75913ccfa9d9fef8660447d4811baf813afd41ff
FredyF3113 <104546111+FredyF3113@users.noreply.github.com>
2023-09-23 18:10:38 -0500

    ISuscriptionRepository.java

--- src/main/java/pe/edu/upc/clothingfashion/repositories/ISuscriptionRepository.java
+++ src/main/java/pe/edu/upc/clothingfashion/repositories/ISuscriptionRepository.java
@@ -5,4 +5,11 @@ import org.springframework.stereotype.Repository;
 import pe.edu.upc.clothingfashion.entities.Suscription;
 @Repository
 public interface ISuscriptionRepository extends JpaRepository<Suscription, Integer> {
+   @Query(value="SELECT s.payment_Date, COUNT(id_user)" +
+            "FROM Users u INNER JOIN Suscription s" +
+            "ON u.id_user=s.users_id_user" +
+            "GROUP BY u.id_user,s.payment_Date" +
+            "ORDER BY COUNT(u.id_user)" +
+            "DESC",nativeQuery = true)
+    List<String[]>UserDate();
 }
