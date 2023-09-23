3f1e8540c77ed092295cc4fae11f7a47b163aedb
FredyF3113 <104546111+FredyF3113@users.noreply.github.com>
2023-09-23 18:12:15 -0500

    ISuscriptionService.java

--- src/main/java/pe/edu/upc/clothingfashion/serviceinterfaces/ISuscriptionService.java
+++ src/main/java/pe/edu/upc/clothingfashion/serviceinterfaces/ISuscriptionService.java
@@ -3,4 +3,5 @@ import pe.edu.upc.clothingfashion.entities.Suscription;
 public interface ISuscriptionService {
     public void insert(Suscription suscription);
     public void delete(int idSuscription);
+    List<String[]>getCountUserIdByPaymentDate();
 }
