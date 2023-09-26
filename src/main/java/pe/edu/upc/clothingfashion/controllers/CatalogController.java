3c23351aece700fdcd3880a2ceb7d78b12846506
KevinArnoldMuchaHuanca <136728739+KevinArnoldMuchaHuanca@users.noreply.github.com>
2023-09-25 20:19:16 -0500

    Update CatalogController.java

--- src/main/java/pe/edu/upc/clothingfashion/controllers/CatalogController.java
+++ src/main/java/pe/edu/upc/clothingfashion/controllers/CatalogController.java
@@ -2,15 +2,17 @@ package pe.edu.upc.clothingfashion.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.clothingfashion.dtos.CatalogDTO;
+import pe.edu.upc.clothingfashion.dtos.CatalogRecommendationsDTO;
 import pe.edu.upc.clothingfashion.entities.Catalog;
 import pe.edu.upc.clothingfashion.serviceinterfaces.ICatalogService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/catalogs")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class CatalogController {
     @Autowired
     private ICatalogService catS;
@@ -24,5 +26,24 @@ public class CatalogController {
     public void eliminar(@PathVariable("id") Integer id)
     {
         catS.delete(id);
+    }
+    @GetMapping("/maxcatalog")
+    public List<CatalogRecommendationsDTO> getRecommendationsByCatalog() {
+        List<String[]> countCatalogRecommendations = catS.getCountCatalogRecommendations();
+        List<CatalogRecommendationsDTO> catalogRecommendationsDTOList = new ArrayList<>();
+
+        for (String[] data : countCatalogRecommendations) {
+            if (data.length >= 2) {
+                CatalogRecommendationsDTO catalogRecommendationsDTO = new CatalogRecommendationsDTO();
+                catalogRecommendationsDTO.setId_catalog(Integer.parseInt(data[1]));
+                catalogRecommendationsDTO.setId_recommendations(Integer.parseInt(data[1]));
+                catalogRecommendationsDTO.setRecommendation_title(data[0]);
+                catalogRecommendationsDTO.setCalification(Integer.parseInt(data[1]));
+
+                catalogRecommendationsDTOList.add(catalogRecommendationsDTO);
+            }
+        }
+
+        return catalogRecommendationsDTOList;
     }
 }
