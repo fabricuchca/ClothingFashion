72a5e17db50f6938ca80b449dfd523ad6a2bef0f
romario2341 <108496580+romario2341@users.noreply.github.com>
2023-11-19 13:26:05 -0500

    Add files via upload

--- src/main/java/pe/edu/upc/clothingfashion/repositories/IClothingRepository.java
+++ src/main/java/pe/edu/upc/clothingfashion/repositories/IClothingRepository.java
@@ -11,8 +11,6 @@ public interface IClothingRepository extends JpaRepository<Clothing, Integer> {
     @Query("SELECT s.nameSeason,c.nameTypeClothing\n" +
             "FROM Season s \n" +
             "INNER JOIN Type_Clothing c ON s.idSeason= c.idTypeClothing")
-
-
     List<Clothing>findByNameSeasonAndNameTypeClothing(String nameSeason,String nameTypeClothing);
     List<Clothing> findByColor(int color);
     List<Clothing> findBySeason(int season);
@@ -20,5 +18,11 @@ public interface IClothingRepository extends JpaRepository<Clothing, Integer> {
     List<Clothing> findByBrand(int brand);
     List<Clothing> findByEvent(int event);
     List<Clothing> findByTypeClothing(int typeClothing);
+    @Query(value = "SELECT s.name_season, COUNT(c.id_season)" +
+            "            FROM Season s inner join Clothing c" +
+            "            ON s.id_season = c.id_season" +
+            "            GROUP BY s.name_season" +
+            "            order BY COUNT(c.id_season)", nativeQuery = true)
+    List<String[]> getCountClothingBySeason();
 
 }
