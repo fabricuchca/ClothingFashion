95ceeb33cfc4050aed5e0ea2f7cc24d1132f34c5
romario2341 <108496580+romario2341@users.noreply.github.com>
2023-11-19 13:20:58 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/clothingfashion/dtos/ClothingSeasonDTO.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.clothingfashion.dtos;
+
+public class ClothingSeasonDTO {
+    private String nameSeason;
+    private int quantityClothing;
+
+    public String getNameSeason() {
+        return nameSeason;
+    }
+
+    public void setNameSeason(String nameSeason) {
+        this.nameSeason = nameSeason;
+    }
+
+    public int getQuantityClothing() {
+        return quantityClothing;
+    }
+
+    public void setQuantityClothing(int quantityClothing) {
+        this.quantityClothing = quantityClothing;
+    }
+}
